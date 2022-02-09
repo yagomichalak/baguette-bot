392f17e322d62f2bf6f8f5a3e558cbba450de214
Yago Michalak <yagomichalak@gmail.com>
2022-02-09 09:39:53 -0300

    IMPROVED the logic of invite advertisement

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -228,6 +228,46 @@ class Moderation(*moderation_cogs):
 			await message.delete()
 			return await message.author.send("**Please, stop sending invites! (Invite Advertisement)**")
 
+		# Invite tracker
+		msg = str(message.content)
+		if 'discord.gg/' in msg.lower() or 'discord.com/invite/' in msg.lower():
+			invite_root = 'discord.gg/' if 'discord.gg/' in msg.lower() else 'discord.com/invite/'
+			ctx = await self.client.get_context(message)
+			if not await utils.is_allowed(allowed_roles).predicate(ctx):
+				is_from_guild = await self.check_invite_guild(msg, message.guild, invite_root)
+
+				if not is_from_guild:
+					await message.delete()
+					return await message.author.send("**Please, stop sending invites! (Invite Advertisement)**")
+
+	async def check_invite_guild(self, msg, guild, invite_root: str):
+		""" Checks whether it's a guild invite or not. """
+
+		start_index = msg.index(invite_root)
+		end_index = start_index + len(invite_root)
+		invite_hash = ''
+		for c in msg[end_index:]:
+			if c == ' ':
+				break
+
+			invite_hash += c
+
+		for char in ['!', '@', '.', '(', ')', '[', ']', '#', '?', ':', ';', '`', '"', "'", ',', '{', '}']:
+			invite_hash = invite_hash.replace(char, '')
+		invite = invite_root + invite_hash
+		inv_code = discord.utils.resolve_invite(invite)
+		if inv_code == 'lesalonfrancais':
+			return True
+
+		if inv_code in []:
+			return True
+
+		guild_inv = discord.utils.get(await guild.invites(), code=inv_code)
+		if guild_inv:
+			return True
+		else:
+			return False
+
 
 	async def check_cache_messages(self, ctx: commands.Context, message: discord.Message) -> None:
 		""" Checks the user who used a banned word. 
