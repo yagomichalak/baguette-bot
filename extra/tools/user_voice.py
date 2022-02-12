4664710619330d7741b6a361c3ff55c12f79b96b
Yago Michalak <yagomichalak@gmail.com>
2022-02-12 20:45:39 -0300

    FIXED an internal bug of when not having a user voice account

--- extra/tools/user_voice.py
+++ extra/tools/user_voice.py
@@ -1,11 +1,15 @@
 import discord
+from discord import slash_command, Option
 from discord.ext import commands
 from mysqldb import the_database
-from typing import List, Union
+
+from typing import List, Union, Optional
 from extra import utils
 import os
+import asyncio
 
-afk_channel_id = int(os.getenv('AFK_CHANNEL_ID'))
+afk_channel_id: int = int(os.getenv('AFK_CHANNEL_ID'))
+guild_ids: List[int] = [int(os.getenv('SERVER_ID'))]
 
 class UserVoiceSystem(commands.Cog):
     """ Cog for the inner systems of UserVoice events. """
@@ -55,6 +59,8 @@ class UserVoiceSystem(commands.Cog):
         if not user_info and not after.self_mute:
             return await self.insert_user_voice(member.id, current_ts)
 
+        if not user_info:
+            return
 
         # Join
         if ac and not bc:
@@ -69,7 +75,7 @@ class UserVoiceSystem(commands.Cog):
             if people_in_vc < 2 or after.self_mute or after.mute or after.deaf or after.channel.id == afk_channel_id:
                 return await self.update_user_voice_time(member.id, 0, current_ts)
 
-            increment: int = current_ts - user_info[3]
+            increment: int = current_ts - user_info[2]
 
             await self.update_user_voice_time(member.id, increment, current_ts)
 
@@ -80,7 +86,7 @@ class UserVoiceSystem(commands.Cog):
                 return await self.update_user_voice_timestamp(member.id, current_ts)
 
 
-            increment: int = current_ts - user_info[3]
+            increment: int = current_ts - user_info[2]
             people_in_vc: int = len([m for m in bc.members if not m.bot and m.id])
             if people_in_vc < 2 or after.self_mute or after.self_deaf:
                 return await self.update_user_voice_time(member.id, increment, None)
@@ -94,7 +100,7 @@ class UserVoiceSystem(commands.Cog):
             if not after.self_mute and not after.self_deaf and not after.mute and not after.deaf and after.channel.id != afk_channel_id:
                 return await self.update_user_voice_timestamp(member.id, current_ts)
 
-            increment: int = current_ts - user_info[3] # Fix this, index out of range sometimes
+            increment: int = current_ts - user_info[2] # Fix this, index out of range sometimes
             people_in_vc: int = len([m for m in bc.members if not m.bot and m.id])
             if people_in_vc < 2 or after.self_mute or after.self_deaf:
                 return await self.update_user_voice_time(member.id, increment, None)
@@ -108,7 +114,7 @@ class UserVoiceSystem(commands.Cog):
                 return await self.update_user_voice_timestamp(member.id, current_ts)
 
 
-            increment: int = current_ts - user_info[3]
+            increment: int = current_ts - user_info[2]
             people_in_vc: int = len([m for m in bc.members if not m.bot and m.id])
             if people_in_vc < 2 or after.mute or after.deaf:
                 return await self.update_user_voice_time(member.id, increment, None)
@@ -122,7 +128,7 @@ class UserVoiceSystem(commands.Cog):
                 return await self.update_user_voice_timestamp(member.id, current_ts)
 
 
-            increment: int = current_ts - user_info[3]
+            increment: int = current_ts - user_info[2]
             people_in_vc: int = len([m for m in bc.members if not m.bot])
             if people_in_vc < 2 or after.mute or after.deaf:
                 return await self.update_user_voice_time(member.id, increment, None)
@@ -132,12 +138,67 @@ class UserVoiceSystem(commands.Cog):
         # Leave
         elif bc and not ac:
 
+            increment: int = current_ts - user_info[2]
             people_in_vc: int = len([m for m in bc.members if not m.bot])
             if people_in_vc < 2 or before.self_mute or before.mute or before.deaf or before.channel.id == afk_channel_id:
                 return await self.update_user_voice_timestamp(member.id, None)
         
 
             await self.update_user_voice_time(member.id, increment)
+
+    @slash_command(name="voice_time", guild_ids=guild_ids)
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def _voice_time_slash_command(self, ctx, 
+        member: Option(discord.Member, name="member", description="The member from whom to show the voice time.", required=False)
+    ) -> None:
+        """ Shows someone's voice time. """
+
+        await ctx.defer()
+
+        if not member:
+            member = ctx.author
+
+        await self._voice_time_callback(ctx, member)
+
+    @commands.command(name="voice_time")
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def _voice_time_command(self, ctx, member: Optional[Union[discord.Member, discord.User]] = None) -> None:
+        """ Shows someone's voice time.
+        :param member: The member from whom to show the voice time. [Optional][Default=You] """
+
+        if not member:
+            member = ctx.author
+
+        await self._voice_time_callback(ctx, member)
+
+
+    async def _voice_time_callback(self, ctx: discord.PartialMessageable, member: Union[discord.Member, discord.User]) -> None:
+        """ Callback for the voice_time command.
+        :param ctx: The context of the command.
+        :param member: The member for whom to show the voice time. """
+
+        answer: discord.PartialMessageable = ctx.send if isinstance(ctx, commands.Context) else ctx.respond
+
+        current_time = await utils.get_time()
+
+        if not (user_voice := await self.get_user_voice(member.id)):
+            await self.insert_user_voice(member.id, current_time.timestamp())
+            await asyncio.sleep(0.3)
+            user_voice = await self.get_user_voice(member.id)
+
+        m, s = divmod(user_voice[1], 60)
+        h, m = divmod(m, 60)
+
+
+        embed = discord.Embed(
+            description=f"**Voice Time:**\n{h:d} hours, {m:02d} minutes and {s:02d} seconds.",
+            color=member.color,
+            timestamp=current_time
+        )
+        embed.set_thumbnail(url=member.display_avatar)
+        embed.set_footer(text=f"Requested by: {ctx.author}", icon_url=ctx.author.display_avatar)
+
+        await answer(embed=embed)
 
 
 class UserVoiceTable(commands.Cog):
@@ -151,11 +212,11 @@ class UserVoiceTable(commands.Cog):
     # ===== Discord commands =====
     @commands.command(hidden=True)
     @commands.has_permissions(administrator=True)
-    async def create_table_server_activity(self, ctx):
+    async def create_table_user_voice(self, ctx):
         """ (ADM) Creates the UserVoice table. """
 
         await ctx.message.delete()
-        if await self.check_user_server_activity_table_exists():
+        if await self.check_user_user_voice_table_exists():
             return await ctx.send("The `UserVoice` already exists!**")
 
         mycursor, db = await the_database()
@@ -173,12 +234,12 @@ class UserVoiceTable(commands.Cog):
 
     @commands.command(hidden=True)
     @commands.has_permissions(administrator=True)
-    async def drop_table_server_activity(self, ctx):
+    async def drop_table_user_voice(self, ctx):
         """ (ADM) Drops the UserVoice table. """
 
         await ctx.message.delete()
 
-        if not await self.check_user_server_activity_table_exists():
+        if not await self.check_user_user_voice_table_exists():
             return await ctx.send("The `UserVoice` doesn't exist!**")
 
         mycursor, db = await the_database()
@@ -190,11 +251,11 @@ class UserVoiceTable(commands.Cog):
 
     @commands.command(hidden=True)
     @commands.has_permissions(administrator=True)
-    async def reset_table_server_activity(self, ctx):
+    async def reset_table_user_voice(self, ctx):
         """ (ADM) Resets the UserVoice table. """
 
         await ctx.message.delete()
-        if not await self.check_user_server_activity_table_exists():
+        if not await self.check_user_user_voice_table_exists():
             return await ctx.send("The `UserVoice` doesn't exist yet!**")
 
         mycursor, db = await the_database()
@@ -205,7 +266,7 @@ class UserVoiceTable(commands.Cog):
 
     # ===== SHOW =====
 
-    async def check_user_server_activity_table_exists(self) -> bool:
+    async def check_user_user_voice_table_exists(self) -> bool:
         """ Checks whether the UserVoice table exists. """
         
         mycursor, _ = await the_database()
